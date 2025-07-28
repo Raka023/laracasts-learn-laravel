0ee49b1ad6c931733041c65bffaf1c7d6601b88e
Raka <143157921+Raka023@users.noreply.github.com>
2025-07-28 22:40:41 +0700

    creating a layout file component

---
+++ resources/views/components/nav-link.blade.php
@@ -0,0 +1,5 @@
+<nav>
+    <a href="/" class="underline">Home</a>
+    <a href="/about" class="underline">About</a>
+    <a href="/contact" class="underline">Contact</a>
+</nav>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
 <x-layout>
     <x-nav-link />
-    <h1 class="font-bold">About</h1>
+    <h1 class="font-bold">Home</h1>
 </x-layout>
