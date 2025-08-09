67e79fd361e61d1f64031c90106b41958ba58156
Raka <143157921+Raka023@users.noreply.github.com>
2025-08-09 21:26:49 +0700

    Add Laravel Debugbar, adding a eager loading for fixing the N+1 problem, and disable lazy loading

--- resources/views/jobs.blade.php
+++ resources/views/jobs.blade.php
@@ -1,12 +1,13 @@
 <x-layout>
     <x-slot:heading>Job Listings</x-slot:heading>
-    <ul>
+    <div class="columns-2 space-y-2">
         @foreach ($jobs as $job)
-            <li>
-                <a href="/jobs/{{ $job['id'] }}" class="hover:underline">
-                    <span class="font-semibold">{{ $job['title'] }}</span>: Pays {{ $job['salary'] }} per month
-                </a>
-            </li>
+            <a href="/jobs/{{ $job['id'] }}" class="block px-4 py-6 border border-gray-300 bg-gray-100 rounded-md hover:bg-zinc-200 transition">
+                <div class="font-bold text-sky-500 text-sm">{{ $job->employer->name }}</div>
+                <div>
+                    <span class="font-bold">{{ $job['title'] }}</span>: Pays {{ $job['salary'] }} per month
+                </div>
+            </a>
         @endforeach
-    </ul>
+    </div>
 </x-layout>
